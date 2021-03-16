ad4aac72d5bf5e2244def220551f410577406bf5
wallacy <wallacevieirada88@gmail.com>
2021-03-16 15:35:20 -0300

    first commit

--- index.js
+++ index.js
@@ -2,12 +2,23 @@
 // passar o caminho do texto via terminal 
 // usar o puppeter, libs padrões do javascript e o ffmpeg
 
-const puppeter = require('puppeteer');
+const puppeteer = require('puppeteer');
 const fs = require('fs');
 var readlineSync = require('readline-sync');
 
 // pega o caminho do arquivo usando o módeulo readline e passa pra uma váriavel
 
-var caminho = readlineSync.question('caminho do documento: ');
+// var caminho = readlineSync.question('caminho do documento: ');
 
 
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: false,
+    });
+    const page = await browser.newPage();
+    await page.goto('https://ttsmp3.com/');
+
+    await page.click('');
+
+    // await browser.close();
+})();
